dc3e113f00cc23dcef230a29b9e5397efc294561
shital bramhe <shitalbramhe@gmail.com>
2021-11-11 12:24:37 +0530

    [shital] Add : Calculated wages for month

--- EmployeeWage/EmployeeWage/EmpWage.cs
+++ EmployeeWage/EmployeeWage/EmpWage.cs
@@ -8,26 +8,34 @@ namespace EmployeeWage
 {
     internal class EmpWage
     {
-        public const int IS_PART_TIME = 1, IS_FULL_TIME = 2, EMP_RATE_PER_HOUR = 20;
-        int empHrs = 0, empWage = 0;
-        public void DailyEmployeeWage()
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
+        public const int NUM_OF_WORKING_DAYS = 20;
+        int empHrs = 0, empWage = 0, totalEmpWage;
+        public void MonthlyEmpWage()
         {
-            Random random = new Random();
-            int empCheck = random.Next(0, 3);
-            switch (empCheck)
+            for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
             {
-                case IS_PART_TIME:
-                    empHrs = 4;
-                    break;
-                case IS_FULL_TIME:
-                    empHrs = 8;
-                    break;
-                default:
-                    empHrs = 0;
-                    break;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                empWage = empHrs * EMP_RATE_PER_HOUR;
+                totalEmpWage += empWage;
+                Console.WriteLine("Employee Wage : " + empWage);
             }
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Employee Wage : " + empWage);
+            Console.WriteLine("Total Employee Wage : " + totalEmpWage);
         }
     }
 }
