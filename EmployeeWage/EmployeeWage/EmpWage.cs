8bb03cbf5b3690d9b9f4b25f043f0d5813490d22
shital bramhe <shitalbramhe@gmail.com>
2021-11-11 12:06:30 +0530

    [shital] Add :Part time Employee wage

--- EmployeeWage/EmployeeWage/EmpWage.cs
+++ EmployeeWage/EmployeeWage/EmpWage.cs
@@ -8,22 +8,27 @@ namespace EmployeeWage
 {
     internal class EmpWage
     {
-        public const int IS_FULL_TIME = 1, EMP_RATE_PER_HOUR = 20;
-        int empHrs = 0, empWage = 0;
-        public void DailyEmployeeWage()
-        {
-            Random random = new Random();
-            int empCheck = random.Next(0, 2);
-            if (empCheck == IS_FULL_TIME)
+            public const int IS_PART_TIME = 1, IS_FULL_TIME = 2, EMP_RATE_PER_HOUR = 20;
+            int empHrs = 0, empWage = 0;
+            public void DailyEmployeeWage()
             {
-                empHrs = 8;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                if (empCheck == IS_PART_TIME)
+                {
+                    empHrs = 4;
+                }
+                else if (empCheck == IS_FULL_TIME)
+                {
+                    empHrs = 8;
+                }
+                else
+                {
+                    empHrs = 0; ;
+                }
+                empWage = empHrs * EMP_RATE_PER_HOUR;
+                Console.WriteLine("Employee Wage : " + empWage);
             }
-            else
-            {
-                empHrs = 0; ;
-            }
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Employee Wage : " + empWage);
         }
     }
 }
