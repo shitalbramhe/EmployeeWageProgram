384351a03db0206fa383a8ef616bf1a71306dfb1
shital bramhe <shitalbramhe@gmail.com>
2021-11-11 07:47:43 +0530

    [shital] Add : initial

---
+++ EmployeeWage/EmployeeWage/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EmployeeWage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcom to the Employee Wage Problem");
+            EmpWage employee = new EmpWage();
+            employee.EmployeeAttendance();
+        }
+    }
+}
